b5d1ed4d4650485fe9eea8848e9f745455fe4fa1
AndressaSales <andressaccs2001@gmail.com>
2024-10-14 18:33:38 -0300

    finalizando o projeto de receita

--- src/Components/Modal.tsx
+++ src/Components/Modal.tsx
@@ -12,9 +12,9 @@ export default function Modal({isOpen, onClose, children} : ModalP){
 
     return(
         <>
-            <div className="fixed inset-0 bg-[#000] bg-opacity-25 backdrop-blur-sm flex justify-center items-center">
+            <div className="fixed inset-0 bg-[#000] bg-opacity-25 backdrop-blur-sm flex justify-center z-[99] items-center">
 
-                <div className="w-[600px] flex flex-col bg-[#fff] rounded-md ">
+                <div className="w-[600px] flex flex-col bg-[#fff] rounded-md min-w-[90%] md:min-w-[600px]">
                     <div className="flex items-center justify-between">
                         <h3 className="pl-4 font-light text-[22px]">Receitas</h3>
                         <button className=" text-[#fff] bg-[#f00] w-[30px] text-center mr-4  px-[6px] rounded-md font-bold text-xl" onClick={onClose}>X</button>
--- src/Components/SearchReceitas.tsx
+++ src/Components/SearchReceitas.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react"
 import Modal from "./Modal"
+import Link from "next/link"
 
 export default function SearchReceitas(){
 
@@ -10,27 +11,44 @@ export default function SearchReceitas(){
         const res = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${buscarIngredientes}`)
         const ingredient = await res.json()
 
-        //console.log(ingredient)
+        //console.log(ingredient.meals)
         setRecibeIngedient(ingredient.meals)
        
     }
 
-    useEffect(() => {
-        search();
-    }, [])
-
     const [modalOpen, setModalOpen] = useState<boolean>(false)
-    const idDetalhes = []
-    
+    const [recipesDetails, setRecipesDetails] = useState([])
+
     async function handleOpenModal(id:any) {
 
         const res = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
         const data = await res.json()
+        const recipe = data.meals[0]
 
-        console.log(id)
-        console.log(data.meals)
+        let ingredientList = ''
+
+        for(let i = 1; i <= 20; i++){
+            const ingre = recipe[`strIngredient${i}`];
+            const measure = recipe[`strMeasure${i}`]
+
+            if(ingre){
+                ingredientList += `<li>${ingre} -${measure}</li>`
+            }else{
+                break
+            }
+        }
+
+        //console.log(id)
+        //console.log(recepe)
+        setRecipesDetails([recipe])
         setModalOpen(!modalOpen)
     }
+
+
+
+    useEffect(() => {
+        search();
+    }, [])
 
     return(
         <main className="bg-[#f4f4f4e9] w-[100%] h-[100vh]">
@@ -52,7 +70,7 @@ export default function SearchReceitas(){
                 {
                     reciveIngedient.map((i) => {
                      return(
-                        <div key={i.idMeal} className="border-2 border-[#ddd] py-4 cursor-pointer md:px-[10px] rounded-md b" onClick={() =>  handleOpenModal(`${i.idMeal}`)}>
+                        <div key={i.idMeal} value={recipesDetails} className="border-2 border-[#ddd] py-4 cursor-pointer md:px-[10px] rounded-md b" onClick={() =>  handleOpenModal(`${i.idMeal}`)}>
                             <div className="items-center justify-center flex">
                                 <img src={i.strMealThumb} alt={i.strMeal} className="w-[250px] rounded-md" />
                             </div>
@@ -66,7 +84,23 @@ export default function SearchReceitas(){
             </div>
             <Modal isOpen={modalOpen} onClose={handleOpenModal}>
                 <div>
-                    <h3>Ola</h3>
+                    {
+                        recipesDetails.map((r)=> {
+                            return(
+                                <div key={r.idMeal}>
+                                    <h2 className="font-bold py-[10px] text-2xl text-center">Nome: {r.strMeal}</h2>
+                                    <h3 className="font-bold pl-[10px]">Categoria: <span className="font-normal">{r.strCategory}</span></h3>
+                                    <h3 className="font-bold pl-[10px]">Origem: <span className="font-normal">{r.strArea}</span></h3>
+                                    <div className="items-center justify-center flex">
+                                        <img src={r.strMealThumb} alt={r.strMeal} className="w-[200px] rounded-md object-cover" />
+                                    </div>
+                                    
+                                    <p className="p-4 text-left">{r.strInstructions}</p>
+                                    <p className="text-center pb-6 pt-4 font-bold">Vídeo: <Link className="text-[#f00] hover:underline hover:decoration-[#f00]" href={r.strYoutube}>Assista no Youtube</Link></p>
+                                </div>
+                            )
+                        })
+                    }
                 </div>
             </Modal>
         </main>
